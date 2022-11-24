ca41fc7839ab2963061e3ffc19fdd33b55b210cf
nazmulrony <nhrony65@gmail.com>
2022-11-24 18:21:37 +0600

    users stored

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const cors = require('cors');
+require('dotenv').config()
+// const jwt = require('jsonwebtoken');
+const app = express();
+// const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const port = process.env.PORT || 5000;
+
+//middle wares
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Phone Kinun express server running!')
+
+})
+// const verifyJWT = (req, res, next) => {
+
+//     const authHeader = req.headers.authorization;
+//     if (!authHeader) {
+//         return res.status(401).send({ message: 'Unauthorized access' })
+//     }
+//     const token = authHeader.split(' ')[1];
+//     jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+//         if (err) {
+//             return res.status(403).send({ message: 'Forbidden access' });
+//         }
+//         req.decoded = decoded;
+//         next();
+//     })
+
+// }
+
+//mongo db connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.cwjhhvi.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+//mongodb crud function
+async function run() {
+    try {
+        //document collections
+        const userCollection = client.db('PhoneKinunDB').collection('users');
+
+        //user data save
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        })
+        //
+
+    }
+    finally {
+
+
+    }
+}
+run().catch(console.dir);
+
+
+
+app.listen(port, () => {
+    console.log(`Phone Kinun server running on port: ${port}`);
+})
